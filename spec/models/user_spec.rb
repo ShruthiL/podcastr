39d61cc1d93b4a25a2103bdcda6fdac7e9ca4354
caellinger <c.a.ellinger@gmail.com>
2020-04-22 14:01:22 -0400

    added tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:first_name).when("First Name", nil, "") }
+  it { should have_valid(:last_name).when("Last Name", nil, "") }
+  it { should have_valid(:user_name).when("User Name") }
+  it { should_not have_valid(:user_name).when(nil, "") }
+  it { should have_valid(:email).when("email@test.com") }
+  it { should_not have_valid(:email).when(nil, "") }
+  it { should have_valid(:password).when("password") }
+  it { should_not have_valid(:password).when(nil, "") }
+  it { should have_valid(:admin).when(true, false) }
 end
