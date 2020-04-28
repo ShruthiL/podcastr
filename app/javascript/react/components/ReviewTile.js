2c306974ba50af79f3db41070adc79a90ce42553
mattmoniz <Monizri@gmail.com>
2020-04-28 16:27:03 -0400

    removed reviews controller, removed link in ReviewTile, added model validation to review model, removed review route from routes.rb

--- app/javascript/react/components/ReviewTile.js
+++ app/javascript/react/components/ReviewTile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const ReviewTile = (props) => {
     return (
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
     validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+    validates :user_id, presence: true
+    validates :podcast_id, presence: true
 
     belongs_to :user
     belongs_to :podcast
