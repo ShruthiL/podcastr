c40a30d05ab9d5b72d03a55f905f44048ecdf17d
Shruthi <sruthiraj.lagisetty@gmail.com>
2020-04-30 14:54:25 -0400

    updated add podcast to check user login

--- app/javascript/react/containers/PodcastsIndexContainer.js
+++ app/javascript/react/containers/PodcastsIndexContainer.js
@@ -4,57 +4,61 @@ import PodcastTile from "../components/PodcastTile";
 
 const PodcastsIndexContainer = (props) => {
   const [podcasts, setPodcasts] = useState([]);
-  const [user, setUser] = useState({user: {
-    user_id: null,
-    user_name: null,
-    admin: null
-  }})
+  const [user, setUser] = useState({
+    user: {
+      user_id: null,
+      user_name: null,
+      admin: null,
+    },
+  });
 
   const fetchPodcasts = () => {
     fetch("/api/v1/podcasts")
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`;
-        let error = new Error(errorMessage);
-        throw error;
-      }
-    })
-    .then((response) => response.json())
-    .then((body) => {
-      setPodcasts(body.podcast);
-      setUser(body.user)
-    })
-    .catch((error) => console.error(`Error in fetch: ${error.message}`));
-  }
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`;
+          let error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .then((response) => response.json())
+      .then((body) => {
+        debugger;
+        ``;
+        setPodcasts(body.podcast);
+        setUser(body.user);
+      })
+      .catch((error) => console.error(`Error in fetch: ${error.message}`));
+  };
 
   useEffect(() => {
-    fetchPodcasts()
+    fetchPodcasts();
   }, []);
 
   let deleteClick = (event, podcast_id) => {
-    event.preventDefault()
-    podcast_id = event.currentTarget.id
+    event.preventDefault();
+    podcast_id = event.currentTarget.id;
     fetch(`/api/v1/podcasts/${podcast_id}`, {
       credentials: "same-origin",
       method: "DELETE",
       headers: {
         "Content-type": "application/json",
-        Accept: "application/json"
-      }
+        Accept: "application/json",
+      },
     })
-    .then((response) => {
-      if (response.ok) {
-        fetchPodcasts()
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`;
-        let error = new Error(errorMessage);
-        throw error;
-      }
-    })
-    .catch((error) => console.error(`Error in fetch: ${error.message}`));
-  }
+      .then((response) => {
+        if (response.ok) {
+          fetchPodcasts();
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`;
+          let error = new Error(errorMessage);
+          throw error;
+        }
+      })
+      .catch((error) => console.error(`Error in fetch: ${error.message}`));
+  };
 
   let podcastTiles;
   if (podcasts.length === 0) {
@@ -65,15 +69,31 @@ const PodcastsIndexContainer = (props) => {
     );
   } else {
     podcastTiles = podcasts.map((podcast) => {
-      return <PodcastTile key={podcast.id} podcast={podcast} user={user.user} deleteClick={deleteClick} />;
+      return (
+        <PodcastTile
+          key={podcast.id}
+          podcast={podcast}
+          user={user.user}
+          deleteClick={deleteClick}
+        />
+      );
     });
   }
 
-  return(
-    <div>
+  let addPodcast;
+  if (user.user_name) {
+    addPodcast = (
       <Link className="button" to="/podcasts/new">
         Add a New Podcast
       </Link>
+    );
+  } else {
+    addPodcast = <></>;
+  }
+
+  return (
+    <div>
+      {addPodcast}
       {podcastTiles}
     </div>
   );
